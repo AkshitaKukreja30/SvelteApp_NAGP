3c9efc1a8f20b024cd84769f89be09c859f2f478
AKSHITA KUKREJA <akshita.kukreja.ext@siemens-healthineers.com>
2023-05-25 12:14:23 +0530

    modified path

--- src/Components/successfulLogin.svelte
+++ src/Components/successfulLogin.svelte
@@ -1,5 +1,5 @@
   <body>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
-    <script type="module" src="\ExternalWebComponent\folder-structure-component.js"></script>
+    <script type="module" src="../Components/ExternalWebComponent/folder-structure-component.js"></script>
     <folder-structure-component></folder-structure-component>
   </body>
